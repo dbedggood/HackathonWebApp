fea822b9cd26b68c2456ca10f85e19521c1b7a33
dbedggood <ddbedggood@gmail.com>
2019-05-04 22:54:22 +1200

    Update getDuration to retrieve time int as seconds

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ function getDuration(eventLat, eventLng) {
                 var results = response.rows[i].elements
                 for (var j = 0; j < results.length; j++) {
                     var element = results[j]
-                    var duration = element.duration.text
+                    var duration = element.duration.value
                     var from = origins[i]
                     var to = destinations[j]
                     console.log(
