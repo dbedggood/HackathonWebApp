00e60b8b74234880874a7db00a43942a4ef45b1f
Daniel Bedggood <ddbedggood@gmail.com>
2019-05-05 12:11:02 +1200

    Split javascript files into two, one for each .html file

--- eventScript.js
+++ eventScript.js
@@ -1,4 +1,15 @@
 const BASE_URL = 'https://hackathon-239523.appspot.com'
+function getEventDetails(id) {
+    fetch('https://hackathon-239523.appspot.com/events/' + id)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(result) {
+        document.getElementById('eventName').innerText = result[0].event_name
+        document.getElementById('eventDate').innerText = result[0].start_time
+        document.getElementById('eventDetails').innerText = result[0].event_description
+    });
+}
 
 function geocodeLocation() {
     geocoder = new google.maps.Geocoder();
@@ -7,6 +18,20 @@ function geocodeLocation() {
       if (status == 'OK') {
         console.log(results[0].geometry.location)
       }
+    })
+}
+
+function drawMap(eventLat, eventLng) {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: eventLat, lng: eventLng },
+        zoom: 15
+    })
+
+    var eventLocation = { lat: eventLat, lng: eventLng }
+    var marker = new google.maps.Marker({
+        position: eventLocation,
+        map: map,
+        title: 'Event Location'
     })
 }
 
@@ -45,38 +70,8 @@ function getDuration(eventLat, eventLng) {
         }
     }
 }
-async function pull_events() {
-    let response = await fetch(BASE_URL + '/events/');
-    let events = await response.json();
 
-    let container = document.querySelector("div#events");
-    events.forEach(i => {
-        let el = document.createElement('a');
-        el.href = `./event.html?event=${i.event_id}`;
-        el.className = 'btn btn-info';
-        el.innerText = i.event_name;
 
-        container.append(el);
-        container.append(document.createElement('hr'));
-    })
-}
-
-async function submit_event() {
-
-    const eventObj = {
-        event_name: document.querySelector("input#event-name").value,
-        start_time: document.querySelector("input#start-time").value,
-        event_description: document.querySelector("textarea#event-description").value
-    };
-    try {
-        let response = await fetch(`${BASE_URL}/events?event=${btoa(JSON.stringify(eventObj))}`, { method: 'post' });
-        alert('Event Successfully Added!');
-    } catch (err) {
-        console.debug(err);
-        alert("An error occured. Sorry about that.");
-    }
-
-}
 async function check_user(){
     if (!localStorage.getItem('person_id')){
         let person_name = prompt("Hi there - It doesn't look like we've seen you before. Enter your name to continue:");
@@ -89,6 +84,4 @@ async function check_user(){
 
 window.onload = () => {
     check_user();
-    pull_events();
-    document.querySelector("button#create-event").addEventListener('click', submit_event);
 }
